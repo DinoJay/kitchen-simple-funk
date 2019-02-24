c2046d0c777c87327217c6300ffcec40f6f4a99a
DinoJay <jott88@googlemail.com>
2019-02-24 21:28:31 +0100

    did not I commit before?

---
+++ src/components/bio.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Image from "gatsby-image"
+
+import { rhythm } from "../utils/typography"
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            className="border border-black"
+            style={{
+              borderTop: '2px solid black',
+              // display: `flex`,
+              // marginBottom: rhythm(2.5),
+            }}
+          >
+            <div className="flex">
+              <div className="unicode text-4xl m-auto">
+                <div className="mr-3">&#127911;</div>
+              </div>
+              <div className="italic">
+                Our monthly radio show on <a title="XL Air radio" src="https://www.xlair.be/">XL Air Radio</a> presented by <strong>{author}</strong>. It's more about music than about cooking! But that doesn't mean we don't have any cheese!
+                {` `}
+        </div>
+          </div>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
+
+export default Bio
---
+++ src/components/layout.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import './styles.css';
+
+import bio from './bio';
+
+import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          className="flex items-center"
+          style={{
+            ...scale(2.2),
+            marginBottom: rhythm(1),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            <span className="" style={{color: 'hsla(0,0%,0%,0.85)'}}>Kitchen /</span>  <br></br><span style={{color: 'tomato'}}>Funk
+              <img className="m-1 pt-2" src="icons/pizza-slice.svg" style={{width: '1em', height: '1em', marginBottom: 0}}></img>
+              <img className="m-1 pt-2" src="icons/antenna.svg" style={{width: '1em', height: '1em', marginBottom: 0}}></img>
+            </span>
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            // fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>{header}</header>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+<div>Icons made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
